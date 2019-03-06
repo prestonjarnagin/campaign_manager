1a803d7a1ce44c9d99e26f0dbc9118d4290d1703
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-05 20:37:43 -0700

    Add spec for missing last name on registration

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -68,5 +68,18 @@ RSpec.feature "Registrations", type: :feature do
       expect(current_path).to eq(new_user_path)
       expect(page).to have_content("Error")
     end
+
+    it 'last name is missing' do
+      fill_in('user_email', :with => '')
+      fill_in('user_password', :with => 'password1')
+      fill_in('user_password_confirmation', :with => 'password1')
+      fill_in('user_first_name', :with => 'Jane')
+      fill_in('user_last_name', :with => '')
+      click_button 'Create Account'
+
+      expect(User.count).to eq(0)
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content("Error")
+    end
   end
 end
