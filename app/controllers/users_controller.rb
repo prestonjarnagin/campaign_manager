ba8eda9e4f305bb821c9d67143391c250af33b95
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-05 20:08:37 -0700

    Add basic logic to handle bad new user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,20 @@ class UsersController < ApplicationController
   end
 
   def create
-      User.create!(user_params)
+    # TODO Add more logic here to tell the user what went wrong if
+    # information wasn't saved successfully
+    user = User.create(user_params)
+    if user.save
       redirect_to dashboard_path
+    else
+      # From the Rails Docs:
+      # Using render with :action is a frequent source of confusion for Rails newcomers.
+      # The specified action is used to determine which view to render, but Rails does not run any of the code for that action in the controller.
+      # Any instance variables that you require in the view must be set up in the current action before calling render.
+      @user = User.new
+      flash[:error] = "Error"
+      render :new
+    end
   end
   private
 
