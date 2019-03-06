461bc93687f815beca72f2155594bab27546bb57
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-05 19:09:39 -0700

    Add spec for happy path user registration

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -8,4 +8,17 @@ RSpec.feature "Registrations", type: :feature do
     expect(current_path).to eq(new_user_path)
   end
 
+  it 'allows for new user signup' do
+    visit new_user_path
+
+    fill_in('user_email', :with => 'jane@doe.com')
+    fill_in('user_password', :with => 'password1')
+    fill_in('user_password_confirmation', :with => 'password1')
+    fill_in('user_first_name', :with => 'Jane')
+    fill_in('user_last_name', :with => 'Doe')
+    click_button 'Create Account'
+
+    expect(User.count).to eq(1)
+    expect(current_path).to eq(dashboard_path)
+  end
 end
