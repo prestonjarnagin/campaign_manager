d8940dbde8fc9e03f04d6b9f1587218172554811
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-05 17:28:47 -0700

    Update routes for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  get '/', to: 'sessions#new'
 
-  get '/', to: 'login#show'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
 end
