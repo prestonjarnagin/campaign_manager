f6fb489727ae8d3adfef1b2a9087c40f73603924
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-05 19:12:47 -0700

    Add basic user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+      User.create!(user_params)
+      redirect_to dashboard_path
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password)
+  end
+
 end
