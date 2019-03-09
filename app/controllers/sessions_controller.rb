bc68b35e131e64e65bf89eba4fea920811edb011
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-09 16:34:50 -0700

    Move login logic from controller to helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: session_params[:email])
-    if user && user.authenticate(session_params[:password])
-      log_in(user)
+    if log_in(session_params)
       redirect_to dashboard_path
     else
-      flash[:error] = "Error" 
+      flash[:error] = "Error"
       render :new
     end
   end
