d3699ee780ffd89984521b47222725e9da6307ba
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-09 16:34:50 -0700

    Move login logic from controller to helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,13 @@
 module SessionsHelper
 
-  def log_in(user)
-    session[:user_id] = user.id
+  def log_in(session_params)
+    user = User.find_by(email: session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      return true
+    else
+      return false
+    end
   end
 
 end
