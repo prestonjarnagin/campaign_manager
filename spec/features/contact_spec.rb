f5906d7649670c0846822e64ffcd9af9b3ec9e24
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-18 14:23:48 -0600

    Add spec for new contact button

--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Contacts' do
-  describe 'Adding' do
-    before(:each) do
-      user = create(:user)
-      allow_any_instance_of(ApplicationController)
-        .to receive(:current_user)
-        .and_return(user)
-      visit dashboard_path
+  before(:each) do
+    user = create(:user)
+    allow_any_instance_of(ApplicationController)
+    .to receive(:current_user)
+    .and_return(user)
+    visit dashboard_path
+  end
+  it 'shows a link from the navbar' do
+    within('nav') do
+      click_link('Contacts')
+      expect(current_path).to eq(contacts_path)
     end
-    it 'shows a link from the navbar' do
-      within('nav') do
-        click_link('Contacts')
-        expect(current_path).to eq(contacts_path)
-      end
+  end
+  describe 'Adding' do
+    it 'show link to add new contact' do
+      click_link('Contacts')
+      click_link('Add New Contact')
+      expect(current_path).to eq new_contact_path
     end
   end
 end
