53dc1ebd8561d1e39ce355bbf9a661551c735fc5
Preston Jarnagin <prstnjarnagin@gmail.com>
2019-03-19 16:36:40 -0600

    Add acceptance testing for creating contacts

--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -15,10 +15,25 @@ RSpec.describe 'Contacts' do
     end
   end
   describe 'Adding' do
-    it 'show link to add new contact' do
+    before(:each) do
       click_link('Contacts')
       click_link('Add New Contact')
+    end
+
+    it 'show link to add new contact' do
       expect(current_path).to eq new_contact_path
+    end
+
+    it 'allows for creation of new contact' do
+      contact = attributes_for(:contact)
+
+      fill_in :contact_name, with: contact[:name]
+      fill_in :contact_internal_name, with: contact[:internal_name]
+      fill_in :contact_phone_number, with: contact[:phone_number]
+      click_on 'Create Contact'
+
+      expect(current_path).to eq(contacts_path)
+      expect(Contact.count).to eq(1)
     end
   end
 end
