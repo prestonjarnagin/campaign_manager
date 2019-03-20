b5e88dc1735b89820fb49068d979245ae4acdb3f
Preston <prstnjarnagin@gmail.com>
2019-03-20 14:50:56 -0600

    Create campaign controller and support files

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,29 @@
+class CampaignsController < ApplicationController
+
+  def index
+    @campaigns = Campaign.all
+  end
+
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+
+    respond_to do |format|
+      if @campaign.save
+        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
+        format.json { render :show, status: :created, location: @campaign }
+      else
+        format.html { render :new }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def campaign_params
+      params.fetch(:campaign, {})
+    end
+end
