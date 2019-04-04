8f9fe7dcd3e396c1a40ec67bd680be382c3c31e9
Preston <prstnjarnagin@gmail.com>
2019-04-04 16:16:11 -0600

    Refactor contact spec and add test for adding related campaigns

--- spec/features/contact_spec.rb
+++ spec/features/contact_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Contacts' do
+  let(:contact) { attributes_for(:contact) }
+
   before(:each) do
     user = create(:user)
     allow_any_instance_of(ApplicationController)
@@ -8,6 +10,7 @@ RSpec.describe 'Contacts' do
     .and_return(user)
     visit dashboard_path
   end
+
   it 'shows a link from the navbar' do
     within('nav') do
       click_link('Contacts')
@@ -25,38 +28,52 @@ RSpec.describe 'Contacts' do
       expect(page).to have_content(contact.internal_name)
       expect(page).to have_content(contact.phone_number)
     end
-
-
   end
 
-  describe 'Adding' do
+  describe 'Contact creation form' do
     before(:each) do
       within('nav') do
         click_link('Contacts')
       end
-      click_link('Add New Contact')
     end
 
-    it 'show link to add new contact' do
+    it 'is accessible from the navbar' do
+      click_link('Add New Contact')
       expect(current_path).to eq new_contact_path
     end
 
-    it 'allows for creation of new contact' do
-      contact = attributes_for(:contact)
+    describe 'allows for' do
+      it 'sucessful creation of new contact' do
+        click_link('Add New Contact')
 
-      fill_in :contact_name, with: contact[:name]
-      fill_in :contact_internal_name, with: contact[:internal_name]
-      fill_in :contact_phone_number, with: contact[:phone_number]
-      click_on 'Create Contact'
+        fill_in_contact_form(contact)
+        click_on 'Create Contact'
 
-      expect(current_path).to eq(contacts_path)
-      expect(Contact.count).to eq(1)
+        expect(current_path).to eq(contacts_path)
+        expect(Contact.count).to eq(1)
+      end
+
+      it 'adding to campaigns at time of creation' do
+        campaign_1 = create(:campaign)
+        campaign_2 = create(:campaign)
+        click_link('Add New Contact')
+
+        fill_in_contact_form(contact)
+        find(:css, "#campaign-#{campaign_1.id}").set(true)
+        find(:css, "#campaign-#{campaign_2.id}").set(true)
+        click_on 'Create Contact'
+
+        subject = Contact.first
+        expect(subject.campaigns.count).to eq(2)
+      end
     end
 
     describe 'doesnt save a new contact if missing...' do
-      it 'name' do
-        contact = attributes_for(:contact)
+      before (:each) do
+        click_link('Add New Contact')
+      end
 
+      it 'name' do
         fill_in :contact_internal_name, with: contact[:internal_name]
         fill_in :contact_phone_number, with: contact[:phone_number]
         click_on 'Create Contact'
@@ -67,8 +84,6 @@ RSpec.describe 'Contacts' do
       end
 
       it 'phone number' do
-        contact = attributes_for(:contact)
-
         fill_in :contact_name, with: contact[:name]
         fill_in :contact_internal_name, with: contact[:internal_name]
         click_on 'Create Contact'
@@ -79,4 +94,10 @@ RSpec.describe 'Contacts' do
       end
     end
   end
+end
+
+def fill_in_contact_form(contact)
+  fill_in :contact_name, with: contact[:name]
+  fill_in :contact_internal_name, with: contact[:internal_name]
+  fill_in :contact_phone_number, with: contact[:phone_number]
 end
