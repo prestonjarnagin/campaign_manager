3057070e6f53aa0e53c6f278455995c195446c00
Preston <prstnjarnagin@gmail.com>
2019-04-04 16:17:43 -0600

    Add test for Contact#add_campaigns()

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Contact, type: :model do
     it { should have_many(:campaign_contacts) }
     it { should have_many(:campaigns).through(:campaign_contacts) }
   end
+
+  describe 'Instance Methods' do
+    it '#add_campaigns' do
+      contact = create(:contact)
+      campaign_ids = create_list(:campaign, 3).map(&:id)
+
+      contact.add_campaigns(campaign_ids)
+
+      expect(contact.campaigns.count).to eq(3)
+    end
+  end
+
+
 end
