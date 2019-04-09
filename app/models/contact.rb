de288ad301acb2d25fbe7ae164533fa6a6747b1b
Preston <prstnjarnagin@gmail.com>
2019-04-09 14:09:13 -0600

    Update Contact#add_campaigns
    #add_campaigns now logs the current time and creates entries on the 
    contact_messages table with times to send a given message.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -10,8 +10,19 @@ class Contact < ApplicationRecord
   has_many :messages, through: :contact_messages
 
   def add_campaigns(campaign_ids)
+
+    # Associate campagins with this contact
     campaign_ids.each do |id|
       self.campaigns << Campaign.find(id)
+    end
+
+    # Associate messages with this contact on contact_messages with datetimes
+    self.campaigns.each do |campaign|
+      time = Time.now
+      campaign.messages.each do |message|
+        send_time = time + (message.elapse_minutes).minutes
+        ContactMessage.create!(message: message, contact: self, send_time: send_time)
+      end
     end
   end
 
