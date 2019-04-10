2aa715d480843fb8fb0f55d64eefec954a9ebe5a
prestonjarnagin <prstnjarnagin@gmail.com>
2019-04-10 16:56:32 -0600

    Add SMSService::send_single_message
    The phone number formatting is a mess. I couldn't find a gem or go-to 
    regex that would take care of it, so I just built a regex that covers 
    the numbers generated by Faker. In the future, numbers should be 
    normalized to e.164 on their way into the database

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -1,10 +1,29 @@
 class SMSService
 
   def initialize(messages)
-    @messages = messages
+    @_messages = messages
   end
 
-  def send
+  def self.send_single_message(contact_message)
+    # Assuming all contacts are U.S. phone numbers
+    original = contact_message.contact.phone_number
+    country_code = '+1'
+    formatted_number = country_code + original.gsub(/^1\-|^\+1|(x.*)|(\D)/, '')
+    body = contact_message.message.text
+
+    if Rails.env == 'test'
+      from = ENV['TWILIO_FROM_NUMBER']
+      to = '+15005550006'
+    else
+      ENV['TWILIO_FROM_NUMBER']
+      to = formatted_number
+    end
+
+    res = client.api.account.messages.create(
+      from: from,
+      to: to,
+      body: body
+    )
     
   end
 
@@ -13,4 +32,16 @@ class SMSService
       SendSmsWorker.perform_async(contact_message)
     end
   end
+
+
+  private
+
+  def messages
+    @_messages
+  end
+
+  def self.client
+    Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN']
+  end
+
 end
