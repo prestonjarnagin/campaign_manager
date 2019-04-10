c1ffcc4ea00fcbec8860594cece4254342fbbc64
Preston <prstnjarnagin@gmail.com>
2019-04-10 10:32:52 -0600

    Add spec for ContactMessage::message_queue
    2 messages are created that should have been sent 10 and 15 minutes ago 
    and are marked as not yet sent. 2 messages are also created that should 
    have been sent 10 minutes ago and have been marked as sent. 
    ::message_queue should return 2 messages

--- spec/models/contact_message_spec.rb
+++ spec/models/contact_message_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe ContactMessage, type: :model do
     it { should belong_to(:contact) }
     it { should belong_to(:message) }
   end
+
+  describe 'Class Methods' do
+    it '::message_queue' do
+      message_to_send_1 = create(:contact_message, send_time: 10.minutes.ago, sent: false)
+      message_to_send_2 = create(:contact_message, send_time: 15.minutes.ago, sent: false)
+      create_list(:contact_message, 2, send_time: 10.minutes.ago, sent: true)
+
+      result = ContactMessage.message_queue
+
+      expect(result.count).to eq(2)
+      expect(result).to include(message_to_send_1)
+      expect(result).to include(message_to_send_2)
+    end
+  end
 end
