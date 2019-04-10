75ba33c4ab7ec5f23cdf8161852c8e11dc8bb040
Preston <prstnjarnagin@gmail.com>
2019-04-10 10:34:18 -0600

    Create ContactMessage::message_queue
    Returns an ActiveRecord relation containing all messages with sent dates 
    in the past that haven't been sent yet

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -3,4 +3,11 @@ class ContactMessage < ApplicationRecord
   validates_inclusion_of :sent, in: [true, false]
   belongs_to :message
   belongs_to :contact
+
+  def self.message_queue
+    ContactMessage
+      .where('send_time < ?', [Time.now])
+      .where(sent: false)
+  end
+
 end
