4ae937eebf5c860937c953d6034be36fa5fedb7e
Preston <prstnjarnagin@gmail.com>
2019-04-10 10:28:00 -0600

    Add initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,50 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: 'Preston', last_name: 'Jarnagin', email: 'prstnjarnagin@gmail.com', password: 'password')
+user = User.create(
+  first_name: 'Preston',
+  last_name: 'Jarnagin',
+  email: 'prstnjarnagin@gmail.com',
+  password: 'password'
+)
+
+contact = Contact.create(
+  name: 'Preston Jarnagin',
+  internal_name: 'Preston Jarnagin',
+  phone_number: '00000000000'
+)
+
+campaign = Campaign.create(
+  name: "Winter is Coming"
+)
+
+message = Message.create(
+  text: "Hello!",
+  elapse_minutes: 1000,
+  campaign: campaign
+)
+
+contact.campaigns << campaign
+campaign.contacts << contact
+campaign.messages << message
+
+ContactMessage.create(
+  message: message,
+  contact: contact,
+  send_time: Time.now - 5.minutes,
+  sent: true
+)
+
+ContactMessage.create(
+  message: message,
+  contact: contact,
+  send_time: Time.now - 5.minutes,
+  sent: false
+)
+
+ContactMessage.create(
+  message: message,
+  contact: contact,
+  send_time: Time.now + 10.minutes,
+  sent: false
+)
