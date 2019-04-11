c8231233e5e99dadb3f7f77c4ae59500e9966c21
prestonjarnagin <prstnjarnagin@gmail.com>
2019-04-11 13:32:01 -0600

    Add logic to ApplicatonController for checking if a user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,17 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(id: uid) if uid
   end
 
+  def require_login
+    unless current_user
+      render_404
+    end
+  end
+
   private
+
+  def render_404
+    render :file => "#{Rails.root}/public/404.html",  layout: false, status: :not_found
+  end
 
   def uid
     session[:user_id]
