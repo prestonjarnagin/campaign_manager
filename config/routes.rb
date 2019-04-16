22da227ca65bcd0b4b8441eb06c071806df38f6b
prestonjarnagin <prstnjarnagin@gmail.com>
2019-04-16 11:25:04 -0600

    Direct new user to url '/signup'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get '/', to: 'sessions#new'
+  get '/signup', to: 'users#new'
   get 'login', to: 'sessions#new'
 
   get '/dashboard', to: 'dashboard#show'
