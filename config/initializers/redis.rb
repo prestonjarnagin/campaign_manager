ebadcc6bf9d4a02c54d03d4e14192f64d6e790aa
prestonjarnagin <prstnjarnagin@gmail.com>
2019-04-17 14:09:56 -0600

    More updates

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-  $redis = Redis.new(url: ENV["REDIS_URL"])
+  REDIS = Redis.new(url: ENV["REDIS_URL"])
 else
-  $redis = Redis.new(host: "10.0.1.1", port: 6380, db: 15)
+  REDIS = Redis.new(host: "10.0.1.1", port: 6380, db: 15)
 end
