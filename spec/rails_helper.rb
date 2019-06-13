795138f543399196e771f633eab22d219cc130bf
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-13 10:12:18 -0600

    Add focus config for RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include FactoryBot::Syntax::Methods
+  config.filter_run_when_matching :focus
 end
 
 Shoulda::Matchers.configure do |config|
