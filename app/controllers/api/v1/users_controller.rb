2cbf059f2dcfe5d0536ae8d1adde72a2d3dbca02
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-19 09:58:14 -0600

    Add basic api/v1 users controller

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::UsersController < ApplicationController
+
+    def create
+        begin
+            User.create!(user_params)
+            response = {
+                "token": 
+            }
+            render json: {}, status: :created
+        end
+        
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)  
+    end
+
+end
