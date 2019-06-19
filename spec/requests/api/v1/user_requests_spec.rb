197871b1005d5481b8a02f7f195d3aaf318eea4d
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-19 09:57:26 -0600

    Add test for user creation API endpoint

---
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "POST /api/v1/users" do
+    it "successfully creates a new user" do
+        payload = {
+            "user": {
+                "email": "user@example.com",
+                "first_name": "Jane",
+                "last_name": "Doe",
+                "password": "password",
+                "password_confirmation": "password",
+            }
+        }
+
+        expect(User.all).to be_empty
+        post("/api/v1/users", 
+            params: payload.to_json,
+            headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' })
+        expect(response).to be_successful
+        expect(response).to have_http_status 201
+    end
+
+    
+end
